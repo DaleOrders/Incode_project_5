2d4b75e637ff2ae7f1ae2232d53a67c93e159f2c
Dale Orders <daleorders7@gmail.com>
2021-06-26 21:45:07 +1000

    added framework fro login, signup pages and the template for the layout page

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,6 @@
 -- create tables
+-- ❒ The schema contains a users table
+-- ❒ The schema contains a ratings table
+-- ❒ The users table contains at least the following fields: ID, email address, password
+-- ❒ The ratings table contains at least the following fields: ID, movie_id (corresponding to the API ID), rating, user_id
+
