5976abdb7d4fa531732026bd1aa1148bb6a7e7fd
Anthony Buccat <a.buccat@ilcloud.com>
2021-06-28 19:31:03 +1000

    database created, users can be seeded db_schema added

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,3 +4,32 @@
 -- ❒ The users table contains at least the following fields: ID, email address, password
 -- ❒ The ratings table contains at least the following fields: ID, movie_id (corresponding to the API ID), rating, user_id
 
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR(255) NOT NULL,
+  firstname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  passwords VARCHAR(64) NOT NULL
+
+);
+
+
+DROP TABLE IF EXISTS ratings;
+CREATE TABLE IF NOT EXISTS ratings (
+   rating_id SERIAL PRIMARY KEY,
+   user_id VARCHAR NOT NULL,
+   movie_id VARCHAR NOT NULL,
+   create_at TIMESTAMPTZ
+   update_at TIMESTAMPTZ
+   rating CHECK(rating_value BETWEEN 0 AND 5),
+   FOREIGN KEY(user_id) 
+   REFERENCES users(user_id)
+   ON DELETE CASCADE
+   
+
+
+
+  
+)
--- sql/seed_tables.sql
+++ sql/seed_tables.sql
@@ -1,1 +1,27 @@
 -- //seed table
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Hanks', 'Tom', 't.hanks@hanks.com', '!THanks1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Washington', 'Denzel', 'd.washington@washington.com', '!DWash1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Harrison', 'Ford', 'h.ford@ford.com', '!HFord1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Harrison', 'Ford', 'h.ford@ford.com', '!HFord1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Bullock', 'Sandra', 's.bullock@bullock.com', '!THanks1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Lawrence', 'Jennifer', 'j.lawrence@lawrence.com', '!JLware1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Pitt', 'Brad', 'b.pitt@pitt.com', '!BPitt1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Jackson', 'Samuel', 's.jackson@jackson.com', '!SJack1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Merly', 'Streep', 's.merly@merly.com', '!MStree1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Blanchett', 'Cate', 'c.blamchett@blamchett.com', '!CBlan1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Rogen', 'Seth', 's.rogen@rogen.com', '!SRoge1');
+INSERT INTO users (surname, firstname, email, passwords)
+VALUES ('Banderas', 'Antonio', 'a.banderas@banderas.com', '!ABard1');
+
+
