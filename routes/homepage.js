cbcec04089db4c25f2b39670c0714327f87ac717
Anthony Buccat <a.buccat@ilcloud.com>
2021-07-07 20:16:27 +1000

    homepage added

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,8 +3,10 @@ const express = require("express");
 const db = require("../database");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('pages/homepage',   {
 
-
-
+    })
+})
 
 module.exports = router;
