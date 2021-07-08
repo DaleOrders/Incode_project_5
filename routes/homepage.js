38a5df6a8dd9427c9bfef61fb984c05d44031729
Anthony Buccat <a.buccat@ilcloud.com>
2021-07-09 09:13:48 +1000

    merge

--- routes/homepage.js
+++ routes/homepage.js
@@ -6,25 +6,15 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('pages/homepage',   {
 
-<<<<<<< HEAD
     })
 })
 
 module.exports = router;
-=======
-<<<<<<< HEAD
 router.get('/', (req,res)=>{
     res.render('pages/homepage')
 })
 
-=======
-
-router.get('/', (req,res)=>{
-    res.render('pages/homepage')
-})
->>>>>>> dale
 
 
 
 module.exports = router
->>>>>>> 0c58725711b83f730cefcf1f083254d7695123ae
