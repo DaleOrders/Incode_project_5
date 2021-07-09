82731c9d2f122b0eb179516e5503dedd47adb383
Anthony Buccat <a.buccat@ilcloud.com>
2021-07-09 16:09:09 +1000

    landing route added

--- routes/landingpage.js
+++ routes/landingpage.js
@@ -4,8 +4,8 @@ const db = require("../database");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('pages/landingpage',   {
-        layout:'./layouts/ls',
+    res.render('pages/landingpage', {
+    layout:'./layouts/nonav',
 
     })
 })
@@ -19,3 +19,4 @@ router.get('/', (req,res)=>{
 
 
 module.exports = router
+
