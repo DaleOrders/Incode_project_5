d7af0c777d19768732b91e6719938ba8442d0a3e
Sunwit Petchoo <sunwit.p@gmail.com>
2021-07-12 17:28:27 +1000

    Add rating start and community rating

--- routes/homepage.js
+++ routes/homepage.js
@@ -9,6 +9,44 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/get-star', (req, res) => {
+   
+    const movieId = req.query.movieId;
+    console.log(movieId)
+    const obj = {
+        avgScore: 0
+      }
+    db.any(
+    "select count(*) as count from ratings where movie_id = $1",
+    [movieId]
+  )
+    .then((results) => {
+      if (results[0].count > 0) {
+        
+        db.any(
+            "select sum(rating)/count(user_id) as avgRating from ratings where movie_id = $1 group by movie_id",
+            [movieId]
+          )
+            .then((data) => {
+             
+              obj['avgScore'] = Number(data[0].avgrating)
+              return res.send(JSON.stringify(obj));
+            })
+            .catch((err) => {
+                //handle error
+                console.log(err)
+            });
+
+      } else {
+        return res.send(JSON.stringify(obj));
+      }
+    })
+    .catch((err) => {
+      res.redirect("/error");
+    });
+    
+})
+
 module.exports = router;
 router.get('/', (req,res)=>{
     res.render('pages/homepage')
